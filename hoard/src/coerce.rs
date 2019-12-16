069eab619fc48250e261784f0ebebd5144d76ad8
Peter Todd <pete@petertodd.org>
2019-12-15 19:40:34 -0500

    Impl basic identity casts

--- hoard/src/coerce.rs
+++ hoard/src/coerce.rs
@@ -73,3 +73,58 @@ where T: TryCast<U>,
         }
     }
 }
+
+unsafe impl TryCastRef<!> for ! {
+    type Error = !;
+    #[inline(always)]
+    fn try_cast_ref(&self) -> Result<&Self, !> {
+        match *self {}
+    }
+}
+
+unsafe impl TryCast<!> for ! {
+    #[inline(always)]
+    fn try_cast(self) -> Result<Self, !> where Self: Sized {
+        match self {}
+    }
+}
+
+unsafe impl TryCastMut<!> for ! {
+    #[inline(always)]
+    fn try_cast_mut(&mut self) -> Result<&mut Self, !> {
+        match *self {}
+    }
+}
+
+macro_rules! unsafe_impl_identity_cast {
+    ($( $t:ty, )*) => {$(
+        unsafe impl TryCastRef<$t> for $t {
+            type Error = !;
+
+            #[inline(always)]
+            fn try_cast_ref(&self) -> Result<&Self, !> {
+                Ok(self)
+            }
+        }
+
+        unsafe impl TryCast<$t> for $t {
+            #[inline(always)]
+            fn try_cast(self) -> Result<Self, !> where Self: Sized {
+                Ok(self)
+            }
+        }
+
+        unsafe impl TryCastMut<$t> for $t {
+            #[inline(always)]
+            fn try_cast_mut(&mut self) -> Result<&mut Self, !> {
+                Ok(self)
+            }
+        }
+    )*}
+}
+
+unsafe_impl_identity_cast! {
+    (), bool,
+    u8, u16, u32, u64, u128,
+    i8, i16, i32, i64, i128,
+}
