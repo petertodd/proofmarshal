245023cb38f81f8ededcbbca833e6d267fab9077
Peter Todd <pete@petertodd.org>
2019-12-31 19:01:44 -0500

    Add an initial derive impl

---
+++ hoard-derive/tests/tests.rs
@@ -0,0 +1,12 @@
+use hoard_derive::Primitive;
+
+#[derive(Primitive)]
+pub struct Foo {
+}
+
+#[cfg(tests)]
+mod tests {
+    #[test]
+    fn test() {
+    }
+}
--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,7 +1,8 @@
 #![feature(never_type)]
 
 use hoard::prelude::*;
-use hoard::marshal::prelude::*;
+use hoard::marshal::*;
+use hoard::marshal::blob::*;
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
@@ -9,64 +10,6 @@ pub struct Outpoint {
     txid: [u8;32],
     n: Le<u32>,
 }
-
-impl<Z> Encoded<Z> for Outpoint {
-    type Encoded = Self;
-}
-
-impl<Z: Zone> Encode<'_, Z> for Outpoint {
-    type State = ();
-
-    #[inline(always)]
-    fn save_children(&self) -> () {}
-
-    #[inline(always)]
-    fn poll<D: Dumper<Z>>(&self, _: &mut (), dumper: D) -> Result<D, D::Error> {
-        Ok(dumper)
-    }
-
-    #[inline(always)]
-    fn encode_blob<W: WriteBlob>(&self, _: &(), dst: W) -> Result<W::Ok, W::Error> {
-        dst.write_primitive(&self.txid)?
-            .write_primitive(&self.n)?
-            .finish()
-    }
-}
-
-impl Persist for Outpoint {
-    type Persist = Self;
-}
-
-impl ValidateBlob for Outpoint {
-    type Error = !;
-
-    #[inline(always)]
-    fn validate_blob<B: BlobValidator<Self>>(blob: B) -> Result<B::Ok, B::Error> {
-        let mut blob = blob.validate_struct();
-        blob.field::<[u8;32],_>(Into::into)?;
-        blob.field::<Le<u32>,_>(Into::into)?;
-        unsafe { blob.assume_valid() }
-    }
-}
-
-unsafe impl<'a,Z> ValidateChildren<'a, Z> for Outpoint {
-    type State = (<[u8;32] as ValidateChildren<'a, Z>>::State,
-                  <Le<u32> as ValidateChildren<'a, Z>>::State);
-
-    #[inline(always)]
-    fn validate_children(this: &'a Self) -> Self::State {
-        (<[u8;32] as ValidateChildren<'a, Z>>::validate_children(&this.txid),
-         <Le<u32> as ValidateChildren<'a, Z>>::validate_children(&this.n))
-    }
-
-    #[inline(always)]
-    fn poll<V: PtrValidator<Z>>(this: &'a Self, state: &mut Self::State, validator: &V) -> Result<&'a Self, V::Error> {
-        <[u8;32] as ValidateChildren<'a, Z>>::poll(&this.txid, &mut state.0, validator)?;
-        <Le<u32> as ValidateChildren<'a, Z>>::poll(&this.n, &mut state.1, validator)?;
-        Ok(unsafe { ::core::mem::transmute(this) })
-    }
-}
-impl<Z> Decode<Z> for Outpoint {}
 
 /*
 #[repr(C)]
