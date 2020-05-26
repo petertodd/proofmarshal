69031a6b452cbfc3df83508659762e1d0166f278
Peter Todd <pete@petertodd.org>
2020-05-26 19:50:03 -0400

    Validate

--- proofmarshal-core/src/lib.rs
+++ proofmarshal-core/src/lib.rs
@@ -1,16 +1,19 @@
 //! Marshalling of merkelized cryptographic proofs - what the cool kids call "blockchain".
 
 #![feature(never_type)]
-#![feature(specialization)]
+#![feature(rustc_attrs)]
 
 #![allow(incomplete_features)]
 #![feature(const_generics)]
 #![feature(const_if_match)]
+#![feature(try_trait)]
 
 #![allow(unused_imports)]
 #![allow(dead_code)]
 
 pub mod commit;
+
+pub mod validate;
 
 #[cfg(test)]
 mod tests {
