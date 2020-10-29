7e49f5a3f12cebad90b8072189dfecc22a9b61fa
Peter Todd <pete@petertodd.org>
2020-10-29 14:24:16 -0400

    Add Blob::to_blob_bytes() convenience functions.

--- hoard/src/blob/mod.rs
+++ hoard/src/blob/mod.rs
@@ -1,4 +1,4 @@
-use std::task::Poll;
+use std::convert::TryFrom;
 use std::marker::PhantomData;
 
 pub mod bytes;
@@ -18,6 +18,23 @@ pub trait Blob : 'static + Sized {
 
     fn encode_bytes<'a>(&self, dst: BytesUninit<'a, Self>) -> Bytes<'a, Self>;
     fn decode_bytes(blob: Bytes<'_, Self>) -> Result<MaybeValid<Self>, Self::DecodeBytesError>;
+
+    /// Convenience wrapper around `encode_bytes`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use hoard::blob::Blob;
+    ///
+    /// assert_eq!(0x1234_u16.to_blob_bytes(),
+    ///            vec![0x34, 0x12]);
+    /// ```
+    fn to_blob_bytes(&self) -> Vec<u8> {
+        let mut r = vec![0u8; Self::SIZE];
+        let dst = BytesUninit::try_from(&mut *r).unwrap();
+        let _ = self.encode_bytes(dst);
+        r
+    }
 }
 
 /// Dynamically sized blob.
@@ -28,6 +45,26 @@ pub unsafe trait BlobDyn : 'static + Pointee + IntoOwned {
 
     fn encode_bytes<'a>(&self, dst: BytesUninit<'a, Self>) -> Bytes<'a, Self>;
     fn decode_bytes(blob: Bytes<'_, Self>) -> Result<MaybeValid<Self::Owned>, Self::DecodeBytesError>;
+
+    /// Convenience wrapper around `encode_bytes`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use hoard::blob::BlobDyn;
+    ///
+    /// // FIXME: use an actual unsized type
+    /// assert_eq!(0x1234_u16.to_blob_bytes_dyn(),
+    ///            vec![0x34, 0x12]);
+    /// ```
+    fn to_blob_bytes_dyn(&self) -> Vec<u8> {
+        let metadata = Self::metadata(self);
+        let size = Self::try_size(metadata).expect("valid metadata");
+        let mut r = vec![0u8; size];
+        let dst = BytesUninit::from_bytes(&mut *r, metadata).unwrap();
+        let _ = self.encode_bytes(dst);
+        r
+    }
 }
 
 unsafe impl<T: Blob> BlobDyn for T {
